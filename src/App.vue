9ce296595b3db236c86809ef740d3467b89429f7
Ana Clara Batista <50646420+anaclaramsb@users.noreply.github.com>
2021-09-22 09:58:08 -0300

    Create component and fetch URL of API

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Conversor moedaA="USD" moedaB="BRL"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Conversor from "./components/Conversor.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Conversor,
   }
 }
 </script>
